55e691668903733cf959c2f48d6bfa19ecbc963e
Blair Nangle <hi@blairnangle.com>
2023-05-20 17:51:24 +0100

    Add type for dictionary to get Exercism to compile code

--- src/resistor-color-duo/resistor-color-duo.ts
+++ src/resistor-color-duo/resistor-color-duo.ts
@@ -1,15 +1,15 @@
 export function decodedValue(colours: string[]): number {
-  const colourToValue = {
-    black: '0',
-    brown: '1',
-    red: '2',
-    orange: '3',
-    yellow: '4',
-    green: '5',
-    blue: '6',
-    violet: '7',
-    grey: '8',
-    white: '9'
+  const colourToValue: {[index: string]: string} = {
+    'black': '0',
+    'brown': '1',
+    'red': '2',
+    'orange': '3',
+    'yellow': '4',
+    'green': '5',
+    'blue': '6',
+    'violet': '7',
+    'grey': '8',
+    'white': '9'
   }
 
   return Number(colours.slice(0, 2).map(c => colourToValue[c]).reduce((l, r) => l + r))
